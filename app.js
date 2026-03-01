29ce3fa706229a072131a49aa0ded8cef7bf1851
nihalgd <nihalgoud.12345@gmail.com>
2026-03-01 20:10:16 +0530

    To-do App

--- app.js
+++ app.js
@@ -1,47 +1,29 @@
-// let todo = [];
-
-// let req = prompt("please enter your request")
-
-// while(true){
-//     if(req == "quit"){
-//         console.log("quitting app");
-//         break;
-//     }
-
-//     if(req == "list"){
-//         console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
-//         for(task of todo){
-//             console.log(task);
-//         }
-//         console.log("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ");
-// } else if(req == "add"){
-//     let task = prompt("Please enter task you want to add");
-//     todo.push(task);
-//     console.log("task added");
-// }
-// } 
 let todo = [];
-
 let req = prompt("please enter your request");
 
-while(true){
-    if(req == "quit"){
+while(true) {
+    if (req == "quit"){
         console.log("quitting app");
         break;
     }
 
-    if(req == "list"){
-        console.log("________________");
-        for(let task of todo){
-            console.log(task);
+    if(req == "list"){ 
+        console.log("_ _ _ _ _ _ _ _ _ _ ");
+        for(let i=0; i<todo.length; i++){
+            console.log(i, todo[i]);
         }
-        console.log("________________");
-    } 
-    else if(req == "add"){
-        let task = prompt("Please enter task you want to add");
+        console.log("_ _ _ _ _ _ _ _ _ _ ");
+    } else if(req == "add"){
+        let task = prompt("please enter the task you want to add");
         todo.push(task);
-        console.log("task added");
+        console.log("task added")
+    } else if(req == "delete"){
+        let idx = prompt("please enter the task index");
+        todo.splice(idx, 1);
+        console.log("task deleted")
+    } else{
+        console.log("wrong request")
     }
 
-    req = prompt("please enter your request"); // important
+    req = prompt("please enter your request");
 }
